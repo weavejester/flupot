0e40ee4af67e11a73539de8e9afcc95d17b4cd68
James Reeves <jreeves@weavejester.com>
2015-05-04 20:54:24 +0100

    Add inline div macro

--- src/flupot/dom.clj
+++ src/flupot/dom.clj
@@ -1,5 +1,8 @@
 (ns flupot.dom)
 
+(defn- dom-symbol [tag]
+  (symbol "js" (str "React.DOM." (name tag))))
+
 (defn- dom-fn [tag]
   `(defn ~tag [opts# & children#]
      (let [args# (cljs.core/array)]
@@ -9,7 +12,10 @@
              (.push args# opts#)))
        (doseq [child# children#]
          (.push args# child#))
-       (.apply ~(symbol "js" (str "React.DOM." tag)) nil args#))))
+       (.apply ~(dom-symbol tag) nil args#))))
 
 (defmacro define-dom-fns [& tags]
   `(do ~@(map dom-fn tags)))
+
+(defmacro div [& children]
+  `(~(dom-symbol 'div) nil nil ~@children))
