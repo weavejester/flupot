62c4b8323081a58d0da1d7157e143d556855974f
James Reeves <jreeves@weavejester.com>
2015-05-05 00:48:04 +0100

    Correct attribute options in DOM macros

--- src/flupot/dom.clj
+++ src/flupot/dom.clj
@@ -1,5 +1,40 @@
 (ns flupot.dom)
 
+(def ^:private attr-opts
+  {:accept-charset :acceptCharset
+   :accesskey :accessKey
+   :allowfullscreen :allowFullScreen
+   :autocomplete :autoComplete
+   :autofocus :autoFocus
+   :autoplay :autoPlay
+   :class :className
+   :colspan :colSpan
+   :contenteditable :contentEditable
+   :contextmenu :contextMenu
+   :crossorigin :crossOrigin
+   :datetime :dateTime
+   :enctype :encType
+   :formaction :formAction
+   :formenctype :formEncType
+   :formmethod :formMethod
+   :formnovalidate :formNoValidate
+   :formTarget :formtarget
+   :hreflang :hrefLang
+   :for :htmlFor
+   :http-equiv :httpEquiv
+   :maxlength :maxLength
+   :mediagroup :mediaGroup
+   :novalidate :noValidate
+   :rowspan :rowSpan
+   :spellcheck :spellCheck
+   :srcdoc :srcDoc
+   :srcset :srcSet
+   :tabindex :tabIndex
+   :usemap :useMap})
+
+(defmacro generate-attr-opts []
+  `(cljs.core/js-obj ~@(mapcat (fn [[k v]] [(name k) (name v)]) attr-opts)))
+
 (defn- dom-symbol [tag]
   (symbol "js" (str "React.DOM." (name tag))))
 
@@ -17,12 +52,14 @@
 (defmacro define-dom-fns [& tags]
   `(do ~@(map dom-fn tags)))
 
+(defn- attrs->react [m]
+  `(cljs.core/js-obj ~@(mapcat (fn [[k v]] [(name (attr-opts k k)) v]) m)))
+
 (defmacro div [opts & children]
   (let [dom-sym (dom-symbol 'div)]
     (if (map? opts)
-      `(~dom-sym (cljs.core/js-obj ~@(mapcat (fn [[k v]] [(name k) v]) opts)) ~@children)
+      `(~dom-sym ~(attrs->react opts) ~@children)
       `(let [opts# ~opts]
          (if (map? opts#)
-           (do (js/console.log "dynamic clj->js")
-               (~dom-sym (cljs.core/clj->js opts#) ~@children))
+           (~dom-sym (attrs->react opts#) ~@children)
            (~dom-sym nil opts# ~@children))))))
--- src/flupot/dom.cljs
+++ src/flupot/dom.cljs
@@ -4,36 +4,7 @@
   (:require cljsjs.react))
 
 (def ^:private attr-opts
-  #js {:accept-charset "acceptCharset"
-       :accesskey "accessKey"
-       :allowfullscreen "allowFullScreen"
-       :autocomplete "autoComplete"
-       :autofocus "autoFocus"
-       :autoplay "autoPlay"
-       :class "className"
-       :colspan "colSpan"
-       :contenteditable "contentEditable"
-       :contextmenu "contextMenu"
-       :crossorigin "crossOrigin"
-       :datetime "dateTime"
-       :enctype "encType"
-       :formaction "formAction"
-       :formenctype "formEncType"
-       :formmethod "formMethod"
-       :formnovalidate "formNoValidate"
-       :formTarget "formtarget"
-       :hreflang "hrefLang"
-       :for "htmlFor"
-       :http-equiv "httpEquiv"
-       :maxlength "maxLength"
-       :mediagroup "mediaGroup"
-       :novalidate "noValidate"
-       :rowspan "rowSpan"
-       :spellcheck "spellCheck"
-       :srcdoc "srcDoc"
-       :srcset "srcSet"
-       :tabindex "tabIndex"
-       :usemap "useMap"})
+  (dom/generate-attr-opts))
 
 (defn- attrs->react [m]
   (reduce-kv
