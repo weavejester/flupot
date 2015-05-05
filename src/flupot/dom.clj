1867c2d025ec3f7009e0a844dd1c5e71e92e782b
James Reeves <jreeves@weavejester.com>
2015-05-05 03:36:20 +0100

    Add inline macros for all elements

--- src/flupot/dom.clj
+++ src/flupot/dom.clj
@@ -1,4 +1,15 @@
-(ns flupot.dom)
+(ns flupot.dom
+  (:refer-clojure :exclude [map meta time]))
+
+(def ^:private tags
+  '[a abbr address area article aside audio b base bdi bdo big blockquote body br
+    button canvas caption cite code col colgroup data datalist dd del details dfn
+    dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5
+    h6 head header hr html i iframe img input ins kbd keygen label legend li link
+    main map mark menu menuitem meta meter nav noscript object ol optgroup option
+    output p param picture pre progress q rp rt ruby s samp script section select
+    small source span strong style sub summary sup table tbody td textarea tfoot th
+    thead time title tr track u ul var video wbr])
 
 (def ^:private attr-opts
   {:accept-charset :acceptCharset
@@ -49,17 +60,24 @@
          (.push args# child#))
        (.apply ~(dom-symbol tag) nil args#))))
 
-(defmacro define-dom-fns [& tags]
-  `(do ~@(map dom-fn tags)))
+(defmacro define-dom-fns []
+  `(do ~@(clojure.core/map dom-fn tags)))
 
 (defn- attrs->react [m]
   `(cljs.core/js-obj ~@(mapcat (fn [[k v]] [(name (attr-opts k k)) v]) m)))
 
-(defmacro div [opts & children]
-  (let [dom-sym (dom-symbol 'div)]
-    (if (map? opts)
-      `(~dom-sym ~(attrs->react opts) ~@children)
-      `(let [opts# ~opts]
-         (if (map? opts#)
-           (~dom-sym (attrs->react opts#) ~@children)
-           (~dom-sym nil opts# ~@children))))))
+(defn- dom-macro [tag]
+  `(let [dom-sym#  '~(dom-symbol tag)
+         opts-sym# (gensym "opts")]
+     (defmacro ~tag [opts# & children#]
+       (if (map? opts#)
+         `(~dom-sym# ~(attrs->react opts#) ~@children#)
+         `(let [~opts-sym# ~opts#]
+            (if (map? ~opts-sym#)
+              (~dom-sym# (attrs->react ~opts-sym#) ~@children#)
+              (~dom-sym# nil ~opts-sym# ~@children#)))))))
+
+(defmacro define-dom-macros []
+  `(do ~@(clojure.core/map dom-macro tags)))
+
+(define-dom-macros)
