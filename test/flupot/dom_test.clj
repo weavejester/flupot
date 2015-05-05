ab152e97b13c5773ffa632eaf801d30a443af619
James Reeves <jreeves@weavejester.com>
2015-05-05 13:39:13 +0100

    Add tests for inline macros

---
+++ test/flupot/dom_test.clj
@@ -0,0 +1,16 @@
+(ns flupot.dom-test
+  (:require [clojure.test :refer :all]
+            [flupot.dom :as dom]))
+
+(deftest test-inline-macros
+  (testing "literal option map"
+    (is (= (macroexpand-1 '(flupot.dom/div {:class "foo"} "bar"))
+           '(js/React.DOM.div (cljs.core/js-obj "className" "foo") "bar"))))
+  (testing "ambiguous option map"
+    (let [sexp (macroexpand-1 '(flupot.dom/span foo bar baz))
+          opts (-> sexp second first)]
+      (is (= sexp
+             `(let [~opts ~'foo]
+                (if (cljs.core/map? ~opts)
+                  (js/React.DOM.span (flupot.dom/attrs->react ~opts) ~'bar ~'baz)
+                  (js/React.DOM.span nil ~opts ~'bar ~'baz))))))))
