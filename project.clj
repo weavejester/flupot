8b26b754a0a554a698d699813ab87d1391f60647
James Reeves <jreeves@weavejester.com>
2015-05-05 15:51:09 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject flupot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ClojureScript functions for creating React elements"
+  :url "https://github.com/weavejester/flupot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
