05e47540325020510570d5dd8fa4594fccb2f5eb
James Reeves <jreeves@weavejester.com>
2016-05-22 22:52:57 +0100

    Add some more examples

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -4,10 +4,14 @@
 
 (enable-console-print!)
 
-(let [app (.getElementById js/document "app")]
+(let [app (.getElementById js/document "app")
+      p   dom/p]
   (br/mount
    (dom/div
     {:class "test"}
     (dom/p "Hello " (dom/strong "World"))
-    (dom/ul (for [i (range 5)] (dom/li {:key i} i))))
+    (p "Testing functions")
+    (p (list "Testing " "functions " "with " "lists"))
+    (dom/ul (for [i (range 1 6)] (dom/li {:key i} i)))
+    (dom/p {:style {:color :red}} "Testing style"))
    app))
