734632e183387a0c460aec8ad046cbc6f7011efd
James Reeves <jreeves@weavejester.com>
2016-05-23 01:28:45 +0100

    Factor out example elements into function

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -4,15 +4,16 @@
 
 (enable-console-print!)
 
-(let [app (.getElementById js/document "app")
-      p   dom/p]
-  (br/mount
-   (dom/div
-    {:class "test"}
-    (dom/p "Hello " (dom/strong "World"))
-    (p "Testing functions")
-    (p (list "Testing " "functions " "with " "lists"))
-    (dom/ul (for [i (range 1 6)] (dom/li {:key i} i)))
-    (dom/p {:style {:color :red}} "Testing style")
-    (dom/p "Testing " (list "lists " (list "within " "lists"))))
-   app))
+(defn content []
+  (let [p dom/p]
+    (dom/div
+     {:class "test"}
+     (dom/p "Hello " (dom/strong "World"))
+     (p "Testing functions")
+     (p (list "Testing " "functions " "with " "lists"))
+     (dom/ul (for [i (range 1 6)] (dom/li {:key i} i)))
+     (dom/p {:style {:color :red}} "Testing style")
+     (dom/p "Testing " (list "lists " (list "within " "lists"))))))
+
+(let [app (.getElementById js/document "app")]
+  (br/mount (content) app))
