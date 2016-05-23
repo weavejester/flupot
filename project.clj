44fa93054db5bdad6464c5a1162bbb04c4933152
James Reeves <jreeves@weavejester.com>
2016-05-23 05:25:17 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145" :scope "provided"]
-                 [cljsjs/react-dom "0.14.0-0"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [cljsjs/react-dom "15.1.0-0"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:test-commands {"unit-tests" ["phantomjs" :runner "target/main.js"]}
    :builds {:main {:source-paths ["src"], :compiler {:output-to "target/main.js"}}}}
